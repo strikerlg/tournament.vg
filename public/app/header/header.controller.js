5551fd403f4178c02713032dd98f11ca5133c133
Wes Copeland <wlcopeland1@gmail.com>
2015-05-26 20:23:21 -0500

    (refactor): move most in-line styles to tournaments.css

--- public/app/header/header.controller.js
+++ public/app/header/header.controller.js
@@ -13,10 +13,10 @@
                 .dropdown({
                     inDuration: 300,
                     outDuration: 225,
-                    constrain_width: false, // Does not change width of dropdown to that of the activator
-                    hover: false, // Activate on hover
-                    gutter: -114, // Spacing from edge
-                    belowOrigin: true // Displays dropdown below the button
+                    constrain_width: false,
+                    hover: false,
+                    gutter: -114,
+                    belowOrigin: true
                 });
         }, 400);
 
